983293240b328680dea9b8013ea50a64f73cd28a
Joao Bogo <joaoluciano9812@gmail.com>
2023-07-21 19:27:14 -0300

    u

--- src/components/KitProducts.js
+++ src/components/KitProducts.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import ProductContainer from "./styles/ProductContainer.styled";
 import { client } from "../client";
 import ProductCard from "./ProductCard";
