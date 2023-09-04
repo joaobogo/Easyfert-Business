39157bc478faa90652b0a0f443cf152476264834
Joao Bogo <joaoluciano9812@gmail.com>
2023-09-04 13:38:59 -0300

    u

--- src/components/styles/Main.styled.js
+++ src/components/styles/Main.styled.js
@@ -1,53 +1,45 @@
 import styled from "styled-components";
 
+const buttonStyles = `
+  position: relative;
+  bottom: 50%;
+  border-radius: 50px;
+  border: white;
+  height: 20px;
+  font-weight: bold;
+  opacity: 80%;
+  cursor: pointer;
+`;
+
+const primaryColor = "#fff";
+const buttonHeight = "20px";
 
 const MainContainer = styled.div`
   display: flex;
   align-items: center;
-  background-color: #fff;
+  background-color: ${primaryColor};
   height: 11.25rem;
   padding: 0;
   justify-content: space-between;
   background: url(${(props) => props.bgimage}) no-repeat;
   background-size: 100vw;
 
+  .leftbutton,
+  .rightbutton {
+    ${buttonStyles}
+    height: ${buttonHeight};
+  }
+
   .leftbutton {
-    position: relative;
-    bottom: 50%;
     left: 10%;
-    border-radius: 50px;
-    border: white;
-    height: 20px;
-    font-weight: bold;
-    opacity: 80%;
   }
-
-
-  .leftbutton:hover {
-    cursor: pointer;
-  }
-
 
   .rightbutton {
-    position: relative;
-    bottom: 50%;
     right: 20%;
-    border-radius: 50px;
-    border: white;
-    height: 20px;
-    font-weight: bold;
-    opacity: 80%;
-  }
-
-  .rightbutton:hover {
-    cursor: pointer;
   }
 
   button {
     width: 80%;
-  }
-
-  @media (max-width: 1250px) {
   }
 
   @media (min-width: 1000px) {
@@ -66,12 +58,8 @@ const MainContainer = styled.div`
 
   @media (max-width: 768px) {
     transform: scale(1.1);
-    
 
-    .leftbutton {
-      display: none;
-    }
-
+    .leftbutton,
     .rightbutton {
       display: none;
     }
